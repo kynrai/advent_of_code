63d1502a4497c3a7e03070f3f4c74d80bc443673
Steven Lee <steven.lee@kynrai.com>
2022-12-11 16:32:19 +0000

    clean up solution a little

--- 2022/day11/main.go
+++ 2022/day11/main.go
@@ -40,26 +40,6 @@ func parseOp(s string) func(int) int {
 				return i + Int(val)
 			}
 		}
-	case "-":
-		if val == "old" {
-			return func(i int) int {
-				return i - i
-			}
-		} else {
-			return func(i int) int {
-				return i - Int(val)
-			}
-		}
-	case "/":
-		if val == "old" {
-			return func(i int) int {
-				return i / i
-			}
-		} else {
-			return func(i int) int {
-				return i / Int(val)
-			}
-		}
 	case "*":
 		if val == "old" {
 			return func(i int) int {
@@ -74,7 +54,6 @@ func parseOp(s string) func(int) int {
 	return nil
 }
 
-// Test: divisible by 13
 func parseTest(s string) func(int) bool {
 	n := Int(strings.Split(strings.TrimSpace(s), " ")[3])
 	return func(i int) bool {
@@ -82,7 +61,7 @@ func parseTest(s string) func(int) bool {
 	}
 }
 
-func one(path string) {
+func parseMonkeys(path string) []*Monkey {
 	input := read.InputAsBatches(path)
 	monkeys := []*Monkey{}
 	for _, v := range input {
@@ -95,17 +74,19 @@ func one(path string) {
 		m.False = Int(strings.Split(strings.TrimSpace(v[5]), " ")[5])
 		monkeys = append(monkeys, m)
 	}
+	return monkeys
+}
+
+func one(path string) {
+	monkeys := parseMonkeys(path)
 
 	for round := 0; round < 20; round++ {
-		for id, m := range monkeys {
+		for _, m := range monkeys {
 			for _, item := range m.Items {
 				newWorry := m.Op(item) / 3
-				fmt.Printf("Round: %d, Monnkey: %d, Starting Worry: %d\n", round, id, item)
 				if m.Test(newWorry) {
-					fmt.Printf("Round: %d, Monnkey: %d, Starting Worry: %d, New Worry: %d, Passing to: %d\n", round, id, item, newWorry, m.True)
 					monkeys[m.True].Items = append(monkeys[m.True].Items, newWorry)
 				} else {
-					fmt.Printf("Round: %d, Monnkey: %d, Starting Worry: %d, New Worry: %d, Passing to: %d\n", round, id, item, newWorry, m.False)
 					monkeys[m.False].Items = append(monkeys[m.False].Items, newWorry)
 				}
 				m.InspectCount++
@@ -121,18 +102,7 @@ func one(path string) {
 }
 
 func two(path string) {
-	input := read.InputAsBatches(path)
-	monkeys := []*Monkey{}
-	for _, v := range input {
-		m := &Monkey{}
-		m.Items = sliceconv.StrToInt(strings.Split(strings.Split(v[1], ": ")[1], ", "))
-		m.Op = parseOp(v[2])
-		m.Test = parseTest(v[3])
-		m.Div = Int(strings.Split(strings.TrimSpace(v[3]), " ")[3])
-		m.True = Int(strings.Split(strings.TrimSpace(v[4]), " ")[5])
-		m.False = Int(strings.Split(strings.TrimSpace(v[5]), " ")[5])
-		monkeys = append(monkeys, m)
-	}
+	monkeys := parseMonkeys(path)
 	lcm := 1
 	for _, m := range monkeys {
 		lcm *= m.Div
@@ -142,12 +112,9 @@ func two(path string) {
 		for _, m := range monkeys {
 			for _, item := range m.Items {
 				newWorry := m.Op(item) % lcm
-				// fmt.Printf("Round: %d, Monnkey: %d, Starting Worry: %d\n", round, id, item)
 				if m.Test(newWorry) {
-					// fmt.Printf("Round: %d, Monnkey: %d, Starting Worry: %d, New Worry: %d, Passing to: %d\n", round, id, item, newWorry, m.True)
 					monkeys[m.True].Items = append(monkeys[m.True].Items, newWorry)
 				} else {
-					// fmt.Printf("Round: %d, Monnkey: %d, Starting Worry: %d, New Worry: %d, Passing to: %d\n", round, id, item, newWorry, m.False)
 					monkeys[m.False].Items = append(monkeys[m.False].Items, newWorry)
 				}
 				m.InspectCount++
@@ -155,7 +122,6 @@ func two(path string) {
 			m.Items = []int{}
 		}
 	}
-	fmt.Println(monkeys[0].InspectCount, monkeys[1].InspectCount, monkeys[2].InspectCount, monkeys[3].InspectCount)
 	sort.Slice(monkeys, func(i, j int) bool {
 		return monkeys[i].InspectCount > monkeys[j].InspectCount
 	})
